5649c00643b430d63cba560418bb06beb5a3e91e
vincethewalker2015 <vincethewalker@yahoo.co.uk>
2020-12-14 14:49:44 +0000

    Set up Redis, and Cart controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
 
   resources :products
   resources :categories
+  #resources :categories, only: [:show]
+  resource :cart, only: [:show] do
+    put 'add/:product_id', to: 'carts#add', as: :add_to
+    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
+    put 'remove_one/:product_id', to: 'carts#removeone', as: :remove_one
+  end
+  
   
 end
