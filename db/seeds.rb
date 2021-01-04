38dde9e7772e99c2042f585da5f52752bf620677
vincethewalker2015 <vincethewalker@yahoo.co.uk>
2021-01-04 14:02:15 +0000

    Disabled Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'Vincent Walker', email: 'vincethewalker@yahoo.co.uk', password: 'password', admin: true)
+# User.create(name: 'Vincent Walker', email: 'vincethewalker@yahoo.co.uk', password: 'password', admin: true)
